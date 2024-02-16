c649455f3199cb40155ba002f5afe0db2a473097
Vaibhav <chauhanvaibhav9602@gmail.com>
2024-02-16 14:39:00 -0500

    Added : Continent Model

---
+++ src/main/java/Models/Continent.java
@@ -0,0 +1,38 @@
+package Models;
+
+public class Continent {
+    Integer d_continentID;
+    String d_continentName;
+    Integer d_continentValue;
+
+    public Continent() {
+
+    }
+    public Continent(Integer d_continentID, String d_continentName, Integer d_continentValue) {
+        this.d_continentID = d_continentID;
+        this.d_continentName = d_continentName;
+        this.d_continentValue = d_continentValue;
+    }
+    public Continent(String d_continentName) {
+        this.d_continentName = d_continentName;
+    }
+
+    public Integer getD_continentID() {
+        return d_continentID;
+    }
+    public void setD_continentID(int p_continentID) {
+        this.d_continentID = p_continentID;
+    }
+    public String getD_continentName() {
+        return d_continentName;
+    }
+    public void setD_continentName(String p_continentName) {
+        this.d_continentName = p_continentName;
+    }
+    public Integer getD_continentValue() {
+        return d_continentValue;
+    }
+    public void setD_continentValue(int p_continentValue) {
+        this.d_continentValue = p_continentValue;
+    }
+}
