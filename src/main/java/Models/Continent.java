4ea938983ad52f53ebcf94ee9c1a06f03fd2ec1b
Vaibhav <chauhanvaibhav9602@gmail.com>
2024-02-16 14:57:38 -0500

    Added : continentID, armies, neighbourCountry List in CountryModel and countries in ContinentModel

--- src/main/java/Models/Continent.java
+++ src/main/java/Models/Continent.java
@@ -4,6 +4,7 @@ public class Continent {
     Integer d_continentID;
     String d_continentName;
     Integer d_continentValue;
+    List<Country> d_countries;
 
     public Continent() {
 
@@ -20,19 +21,27 @@ public class Continent {
     public Integer getD_continentID() {
         return d_continentID;
     }
-    public void setD_continentID(int p_continentID) {
-        this.d_continentID = p_continentID;
-    }
     public String getD_continentName() {
         return d_continentName;
-    }
-    public void setD_continentName(String p_continentName) {
-        this.d_continentName = p_continentName;
     }
     public Integer getD_continentValue() {
         return d_continentValue;
     }
+    public List<Country> getD_countries() {
+        return d_countries;
+    }
+
+    public void setD_continentID(int p_continentID) {
+        this.d_continentID = p_continentID;
+    }
+
+    public void setD_continentName(String p_continentName) {
+        this.d_continentName = p_continentName;
+    }
     public void setD_continentValue(int p_continentValue) {
         this.d_continentValue = p_continentValue;
+    }
+    public void setD_countries(List<Country> p_countries) {
+        this.d_countries = p_countries;
     }
 }
--- src/main/java/Models/Country.java
+++ src/main/java/Models/Country.java
@@ -1,12 +1,22 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Country {
     int d_countryId;
     String d_countryName;
-
-    public Country(int p_countryId, String p_countryName) {
+    int d_continentId;
+    int d_armies;
+    List<Integer> d_neighbourCountryId = new ArrayList<Integer>();
+    public Country(int p_countryId, String p_countryName, int p_continentId) {
         this.d_countryId = p_countryId;
         this.d_countryName = p_countryName;
+        this.d_continentId = p_continentId;
+    }
+    public Country(int p_countryId, int p_continentId) {
+        this.d_countryId = p_countryId;
+        this.d_continentId = p_continentId;
     }
     public Country(String p_countryName) {
         this.d_countryName = p_countryName;
@@ -18,11 +28,29 @@ public class Country {
     public String getD_countryName() {
         return d_countryName;
     }
+    public int getD_continentId() {
+        return d_continentId;
+    }
+    public int getD_armies() {
+        return d_armies;
+    }
+    public List<Integer> getD_neighbourCountryId() {
+        return d_neighbourCountryId;
+    }
 
     public void setD_countryId(int p_countryId) {
         this.d_countryId = p_countryId;
     }
     public void setD_countryName(String p_countryName) {
         this.d_countryName = p_countryName;
+    }
+    public void setD_continentId(int p_continentId) {
+        this.d_continentId = p_continentId;
+    }
+    public void setD_armies(int p_armies) {
+        this.d_armies = p_armies;
+    }
+    public void setD_neighbourCountryId(List<Integer> p_neighbourCountryId) {
+        this.d_neighbourCountryId = p_neighbourCountryId;
     }
 }
