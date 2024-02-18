59f4cd8de8ee6eacd1375e5b25809bc86c0efbf2
Yashesh <yashesh.sorathia@gmail.com>
2024-02-17 20:12:48 -0500

    Updated the Map View file.

--- src/main/java/Views/MapView.java
+++ src/main/java/Views/MapView.java
@@ -36,7 +36,57 @@ public class MapView {
      */
     List<Continent> d_continents;
 
+    /**
+     * Parameterized constructor to initialize Mapview without players.
+     *
+     * @param p_gameState current state of game
+     */
     public MapView(GameState p_gameState) {
         d_gameState = p_gameState;
         d_map = p_gameState.getD_map();
+        d_countries = p_gameState.getD_map().getD_countries();
+        d_continents = p_gameState.getD_map().getD_continents();
+    }
+
+    /**
+     * Parameterized constructor to initialize Mapview with players.
+     *
+     * @param p_gameState current state of game
+     * @param p_players   list of players
+     */
+    public MapView(GameState p_gameState, List<Player> p_players) {
+        d_gameState = p_gameState;
+        d_countries = d_map.getD_countries();
+        d_continents = d_map.getD_continents();
+    }
+
+    /**
+     * Renders a centered string for heading.
+     *
+     * @param p_width  defined width in formatting
+     * @param p_string string to show
+     */
+    private void renderCenteredString(int p_width, String p_string) {
+        String l_centeredString = String.format("%-" + p_width + "s",
+                String.format("%" + (p_string.length() + (p_width - p_string.length()) / 2));
+        System.out.format(l_centeredString + "\n");
+    }
+
+    /**
+     * Separator to separate the heading.
+     */
+    private void renderSeparator() {
+        System.out.format("+%s+%n", "-".repeat(GameConstants.CONSOLE_WIDTH - 2));
+    }
+
+    /**
+     * Render continent name with centered string and separator.
+     *
+     * @param p_continentName continent name to show
+     */
+    private void renderContinentName(String p_continentName) {
+        String l_continentName = p_continentName + " ( " +
+                GameConstants.CONTROL_VALUE + " : " +
+                d_gameState.getD_map().retrieveContinent(p_continentName).getD_continentValue() + " ) ";
+        renderSeparator();
     }
