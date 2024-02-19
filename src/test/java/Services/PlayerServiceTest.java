7935c0ec56cb10a0bd498ac7fcc8231e69532628
Aashvi zala <you@example.com>
2024-02-18 21:55:06 -0500

    added other test cases

--- src/test/java/Services/PlayerServiceTest.java
+++ src/test/java/Services/PlayerServiceTest.java
@@ -2,6 +2,7 @@ package Services;
 
 import Models.*;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -82,6 +83,44 @@ public class PlayerServiceTest {
         PlayerService l_playerService = new PlayerService();
         Boolean l_isUnique=l_playerService.isPlayerNameUnique(l_listOfPlayers,"Prachi");
         assertEquals(false,l_isUnique);
+    }
+
+    /**
+     * test to check that player cannot deploy more armies that there is in
+     * their reinforcement pool
+     */
+    @org.junit.jupiter.api.Test
+    void validateDeployOrderArmies() {
+        Player l_player1= new Player("W3");
+        PlayerService l_playerServices= new PlayerService();
+        l_player1.setD_noOfUnallocatedArmies(15);
+        String l_noOfArmies= "12";
+        Boolean l_isValidDeployableArmy= l_playerServices.validateDeployOrderArmies(l_player1,l_noOfArmies);
+        Assertions.assertEquals(false,l_isValidDeployableArmy);
+
+    }
+    /**
+     * test for the calculation of number of reinforcement armies
+     */
+    @org.junit.jupiter.api.Test
+    void calculateArmiesForPlayer() {
+        Player l_player1= new Player("Lucifer");
+        Continent l_cont= new Continent();
+        Country l_country= new Country("INDIA");
+        l_cont.setD_continentName("ASIA");
+        l_cont.setD_continentID(1);
+        l_cont.setD_continentValue(6);
+        List<Country> l_listOfCountries= new ArrayList<>();
+        l_listOfCountries.add(l_country);
+        l_cont.setD_countries(l_listOfCountries);
+        List<Continent> l_ListOfContinent = new ArrayList<>();
+        l_ListOfContinent.add(l_cont);
+        l_player1.setD_continentsOwned(l_ListOfContinent);
+        l_player1.setD_coutriesOwned(l_listOfCountries);
+        PlayerService l_playerService = new PlayerService();
+        int l_armyNumber=l_playerService.calculateArmiesForPlayer(l_player1);
+        Assertions.assertEquals(9,l_armyNumber);
+
     }
 
     /**
