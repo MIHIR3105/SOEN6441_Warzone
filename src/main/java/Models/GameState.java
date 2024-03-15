42eb83b9cb6366764fd5d79cdb1a5d2c6edaf809
Aashvi zala <you@example.com>
2024-03-15 15:10:33 -0400

    Refactored GameState Class and have added neccessary new changes

--- src/main/java/Models/GameState.java
+++ src/main/java/Models/GameState.java
@@ -12,18 +12,28 @@ public class GameState {
      */
     Map d_map;
 
-
+    /**
+     * Log Entries for existing game state.
+     */
+    LogEntryBuffer d_logEntryBuffer = new LogEntryBuffer();
 
     /**
      * list of all players.
      */
     List<Player> d_players;
-
+    /**
+     * list of unexecuted orders.
+     */
+    List<Order> d_unexecutedOrders;
     /**
      * list of pending orders.
      */
     List<Order> d_pendingOrders;
 
+    /**
+     * Checks if user has used load command.
+     */
+    Boolean d_loadCommand = false;
     /**
      * error message.
      */
@@ -66,6 +76,23 @@ public class GameState {
     }
 
     /**
+     * getter method to get the list of orders which are yet to be executed.
+     *
+     * @return list of orders
+     */
+    public List<Order> getD_unexecutedOrders() {
+        return d_unexecutedOrders;
+    }
+
+    /**
+     * setter method to set the unexecuted orders.
+     *
+     * @param p_unexecutedOrders list of unexecuted orders
+     */
+    public void setD_unexecutedOrders(List<Order> p_unexecutedOrders) {
+        this.d_unexecutedOrders = p_unexecutedOrders;
+    }
+    /**
      * getter method to get the list of pending orders.
      *
      * @return list of orders
@@ -101,13 +128,49 @@ public class GameState {
         this.d_error = p_error;
     }
 
-    @Override
-    public String toString() {
-        return "GameState{" +
-                "d_map=" + d_map +
-                ", d_players=" + d_players +
-                ", d_pendingOrders=" + d_pendingOrders +
-                ", d_error='" + d_error + '\'' +
-                '}';
+    /**
+     * Message to be added in the log.
+     *
+     * @param p_logMessage Log Message to be set in the Object
+     * @param p_logType Type of Log Message to be Added
+     */
+    public void updateLog(String p_logMessage, String p_logType) {
+        d_logEntryBuffer.currentLog(p_logMessage, p_logType);
     }
+
+    /**
+     * Fetches the most recent Log in current GameState.
+     *
+     * @return recent Log Message
+     */
+    public String getRecentLog(){
+        return d_logEntryBuffer.getD_logMessage();
+    }
+
+    /**
+     * Sets the Boolean load map variable.
+     */
+    public void setD_loadCommand() {
+        this.d_loadCommand = true;
+    }
+
+    /**
+     * Returns if load command is used.
+     *
+     * @return bool value if map is loaded
+     */
+    public boolean getD_loadCommand(){
+        return this.d_loadCommand;
+    }
+
+
+//    @Override
+//    public String toString() {
+//        return "GameState{" +
+//                "d_map=" + d_map +
+//                ", d_players=" + d_players +
+//                ", d_pendingOrders=" + d_pendingOrders +
+//                ", d_error='" + d_error + '\'' +
+//                '}';
+//    }
 }
