c208ce7d8d64f26cb9e2236b3e6de565046b9329
Aashvi zala <you@example.com>
2024-03-15 13:15:52 -0400

    Have added Card Interface and made needed changes

---
+++ src/main/java/Models/Card.java
@@ -0,0 +1,17 @@
+package Models;
+
+/**
+ * This model class manages all the cards owned by the player.
+ * @author Aashvi Zala
+ */
+public interface Card extends Order {
+
+    /**
+     * Pre-validation of card type order.
+     *
+     * @param p_gameState Gamestate
+     * @return true or false
+     */
+    public Boolean checkValidOrder(GameState p_gameState);
+
+}
